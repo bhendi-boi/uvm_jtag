89ba0bfdf838a2875004f7298f985f081517e3a0
bhendi-boi <bhendiisgreen@gmail.com>
2025-04-11 11:30:09 +0530

    removed a bug

--- sequence.sv
+++ sequence.sv
@@ -3,8 +3,6 @@ class async_reset_seq extends uvm_sequence;
 
     rand transaction tr;
 
-    constraint reset {tr.trst_pad_i == 1;}
-
     function new(string name = "async_reset_seq");
         super.new(name);
     endfunction
@@ -24,9 +22,6 @@ class sync_reset_seq extends uvm_sequence;
 
     rand transaction tr;
 
-    constraint trst_is_off {tr.trst_pad_i == 0;}
-    constraint tdi_is_1 {tr.tdi_pad_i == 1;}
-
     function new(string name = "sync_reset_seq");
         super.new(name);
     endfunction
@@ -34,11 +29,11 @@ class sync_reset_seq extends uvm_sequence;
     task body();
         tr = transaction::type_id::create("tr");
 
-        repeat (5) begin  // 5 consequtive TDI = 1 resets JTAG
+        repeat (5) begin  // 5 consequtive TMS = 1 resets JTAG
             start_item(tr);
             if (!tr.randomize() with {
                     tr.trst_pad_i == 0;
-                    tr.tdi_pad_i == 1;
+                    tr.tms_pad_i == 1;
                 })
                 `uvm_fatal("Async Reset Sequence", "Randomisation failed")
             finish_item(tr);
