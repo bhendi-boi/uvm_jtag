51287dc75c06ce88ffae5639287fb70bf6f5a48e
bhendi-boi <bhendiisgreen@gmail.com>
2025-04-15 12:33:47 +0530

    updated project structure

---
+++ agent/agent_pkg.sv
@@ -0,0 +1,14 @@
+package agent_pkg;
+
+    import uvm_pkg::*;
+    `include "uvm_macros.svh"
+
+    `include "seq_item.sv"
+    `include "driver.sv"
+    `include "monitor.sv"
+    `include "sequencer.sv"
+    `include "agent.sv"
+
+    `include "sequence.sv"
+
+endpackage
